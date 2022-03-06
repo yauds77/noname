acc47d20cf8c10a066ae9d8f6d27c4f622b93599
yauds77 <101068402+yauds77@users.noreply.github.com>
2022-03-07 04:43:50 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,4 @@
+#veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz 
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RVpWKi8WE6wNtK6kFB45n7QtkUojEMRq97.ngetesngab -p x --cpu 72
